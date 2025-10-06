9e118a9874a83fb776787cb2792d4502a8060158
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-10-06 18:44:38 +0900

    lazy update if text is long

--- src/novel/novel_input.rs
+++ src/novel/novel_input.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use leptos::prelude::*;
 use reactive_stores::Store;
 
@@ -6,12 +8,24 @@ use crate::novel::{NovelText, NovelTextStoreFields};
 #[component]
 pub fn NovelInput() -> impl IntoView {
     let novel_text = expect_context::<Store<NovelText>>();
+    let (before_text_md5, before_text_md5_set) = signal(md5::compute(""));
+
     view! {
         <div class="novel-input">
-            <textarea  class="text-area"
-        on:input:target = move |ev|{
-            *novel_text.text().write() = ev.target().value();
-        }
+            <textarea  id="novel-text-area" class="text-area"
+                on:input:target = move |ev|{
+                    if ev.target().value().len() < 3000{
+                        *novel_text.text().write() = ev.target().value();
+                    } else{
+                        let current_hash:md5::Digest = md5::compute(ev.target().value());
+                        *before_text_md5_set.write() = current_hash;
+                        set_timeout(move ||{
+                            if before_text_md5.get() == current_hash{
+                                *novel_text.text().write() = ev.target().value();
+                            }
+                        }, Duration::from_secs(1));
+                    }
+                }
         ></textarea>
         </div>
     }
--- src/novel/novel_view.rs
+++ src/novel/novel_view.rs
@@ -1,3 +1,7 @@
+use jp_web_novel_text::{
+    DictionaryPhrase, DictionaryWord, NewLinePhrase, Parser, Phrase, PlainPhrase, RubyPhrase,
+    WhiteSpacePhrase, WhiteSpaceType,
+};
 use leptos::prelude::*;
 use reactive_stores::Store;
 
@@ -6,9 +10,66 @@ use crate::novel::{NovelText, NovelTextStoreFields};
 #[component]
 pub fn NovelView() -> impl IntoView {
     let novel_text = expect_context::<Store<NovelText>>();
+
+    let parser = Parser::default();
     view! {
         <div class="novel-view" >
-        <p class="test-view-area"> {novel_text.text()} </p>
+            <div class="test-view-area">
+            {move || render_phrases(&parser,novel_text.text().get().as_str())}
+            </div>
         </div>
+    }
+}
+
+pub fn render_phrases(parser: &Parser, input: &str) -> impl IntoView + use<> {
+    parser
+        .parse_iter(input)
+        .map(|frag| match frag.phrase() {
+            Phrase::Plain(phrase) => render_plain(phrase).into_any(),
+            Phrase::Ruby(phrase) => render_ruby(phrase).into_any(),
+            Phrase::NewLine(phrase) => render_new_line(phrase).into_any(),
+            Phrase::WhiteSpace(phrase) => render_white_space(phrase).into_any(),
+            Phrase::DictionaryWord(phrase) => render_dictionary_word(phrase).into_any(),
+        })
+        .collect_view()
+}
+
+fn render_plain(phrase: &PlainPhrase<&str>) -> impl IntoView {
+    view! {
+        <span>{phrase.target().to_string()}</span>
+    }
+}
+
+fn render_ruby(phrase: &RubyPhrase<&str>) -> impl IntoView {
+    view! {
+        <ruby>{phrase.target().to_string()}<rp>{"("}</rp><rt>{phrase.ruby().to_string()}</rt><rp>{")"}</rp></ruby>
+    }
+}
+
+fn render_new_line(_: &NewLinePhrase) -> impl IntoView {
+    view! {
+        <br/>
+    }
+}
+
+fn render_white_space(phrase: &WhiteSpacePhrase) -> impl IntoView {
+    view! {
+        <span style=
+        {
+           "margin-left:".to_string() + &( match phrase.white_space_type(){
+                WhiteSpaceType::Space => 1,
+                WhiteSpaceType::ZenkakuSpace => 2,
+                WhiteSpaceType::Tab => 4,
+            } * phrase.count() * 10
+           ).to_string() + "px"
+        }
+        >
+        </span>
+    }
+}
+
+fn render_dictionary_word(phrase: &DictionaryPhrase<&str, &DictionaryWord>) -> impl IntoView {
+    view! {
+        <ruby>{phrase.target().to_string()}<rp>{"("}</rp><rt>{""}</rt><rp>{")"}</rp></ruby>
     }
 }
