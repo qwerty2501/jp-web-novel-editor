a9b3132d777378afef767dd3870a9aff5a38d670
qwerty2501 <939468+qwerty2501@users.noreply.github.com>
2025-10-11 21:08:05 +0900

    set assets main.scss

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,12 @@
 //use crate::menu::*;
+use crate::app::document::Stylesheet;
 use crate::novel::*;
 use dioxus::prelude::*;
 
 #[component]
 pub fn App() -> Element {
     rsx! {
+        Stylesheet { href: asset!("assets/main.scss") },
         div{
             class:"app",
             NovelEditor{}
